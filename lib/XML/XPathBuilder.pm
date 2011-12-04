dafc1e3751a575dd8a72216c519afc51e5c19bba
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-12-04 14:36:11 +0900

    initial commit

---
+++ lib/XML/XPathBuilder.pm
@@ -0,0 +1,32 @@
+package XML::XPathBuilder;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+XML::XPathBuilder -
+
+=head1 SYNOPSIS
+
+  use XML::XPathBuilder;
+
+=head1 DESCRIPTION
+
+XML::XPathBuilder is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
